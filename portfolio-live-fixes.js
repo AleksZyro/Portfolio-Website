d84eadd662f4a11dcca334a29e387103a8bf9b8a
Aleksandar Nikolić <Aleksandar09n@gmail.com>
2026-08-17 15:22:00 +0200

    Make project cards clickable

--- portfolio-live-fixes.js
+++ portfolio-live-fixes.js
@@ -41,7 +41,15 @@
 
       .item-card[data-clickable-card="true"] .project-details-trigger,
       .item-card[data-clickable-card="true"] [data-detail-trigger="true"] {
-        display: none !important;
+        position: absolute !important;
+        width: 1px !important;
+        height: 1px !important;
+        padding: 0 !important;
+        margin: -1px !important;
+        overflow: hidden !important;
+        clip: rect(0 0 0 0) !important;
+        white-space: nowrap !important;
+        border: 0 !important;
       }
 
       .item-card[data-project-id="besp2074"] img,
